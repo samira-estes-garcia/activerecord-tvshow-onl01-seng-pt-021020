dc73345c03ff3c95e323ba873b95ab96a8fe68a3
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-24 19:44:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    most_popular_show = self.where("rating = ?", self.highest_rating).first
+    most_popular_show = self.where("rating = ?", self.highest_rating).first 
+    most_popular_show
   end
 
 
